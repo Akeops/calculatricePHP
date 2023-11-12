0e88a0798767d64a0b02fd2ba534bf171f5472ff
Akeops <lalanne.andoni1@gmail.com>
2023-11-12 22:04:09 +0100

    J'ai fini le form

--- index.php
+++ index.php
@@ -6,11 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="POST">
-        <label for="number1">Nombre 1</label>
-        <input type="text" name="number1" placeholder="nombre 1">
+    <form action="calculatrice.php" method="POST">
+        <div class="op1">
+            <label for="number1">Nombre 1</label><br>
+            <input type="text" name="number1" placeholder="nombre 1">
+        </div>
+        
+        <div class="operator">
+            <label for="operator">Operateur</label><br>
+            <select id="operator" name="operator">
+                <option value="+">+</option>
+                <option value="+">-</option>
+                <option value="+">/</option>
+                <option value="+">*</option>
+            </select>
+        </div>
 
+        <div class="op2">
+            <label for="number2">Nombre 2</label><br>
+            <input type="text" name="number2" placeholder="nombre 2">
+        </div>
 
+        <input type="submit" value="Calculer">
 
     </form>
 </body>
